3bba1b98c53163d00cb36994de79044051d933dc
Sean-Garnett-II <68874649+Sean-Garnett-II@users.noreply.github.com>
2022-03-14 11:29:15 -0400

    Edited TheOtherRoles, Added versioning and archiving, swapped xcopy to robocopy.

--- TheOtherRoles.ps1
+++ TheOtherRoles.ps1
@@ -1,20 +1,27 @@
-cmd.exe /c curl https://api.github.com/repos/Eisbison/TheOtherRoles/releases/latest | findstr "browser_download_url" >tmp
-$targetFolder = ".\The Other Roles"
-mkdir $targetFolder
-$url = get-content tmp
-$url = [regex]::Matches($url, 'https:[\/\w\.\d-]+zip').Value
-cmd.exe /c curl -Lo temp.zip $url
-Expand-archive .\temp.zip -destinationpath $targetFolder
+New-Item -Path .\"Previous Versions" -ItemType Directory -Force
+New-Item -Path .\"Previous Versions\The Other Roles" -ItemType Directory -Force
+Get-ChildItem -Path .\ -Filter "*The Other Roles*" | Select-Object -ExpandProperty Name | Move-Item -Destination .\"Previous Versions\The Other Roles"
+cmd.exe /c curl https://api.github.com/repos/Eisbison/TheOtherRoles/releases/latest >TheOtherRoles.txt
+$downloadUrl = Select-String -Path TheOtherRoles.txt -Pattern 'browser_url_download'
+$downloadUrl = [regex]::Matches($downloadUrl, 'https:[\/\w\.\d-]+zip').Value
+$folderName = Select-String -Path TheOtherRoles.txt -Pattern '"name"[ ]?:[ ]?"The Other Roles '
+$folderName = [regex]::Matches($folderName, 'https:[\/\w\.\d-]+zip').Value
+if(Test-Path -Path $folderName){
+    Remove-Item $folderName -Recurse
+}
+New-Item -Path $folderName -ItemType Directory -Force
+cmd.exe /c curl -Lo TheOtherRoles.zip $downloadUrl
+Expand-archive .\TheOtherRoles.zip -destinationpath $folderName
+del TheOtherRoles.txt
+del TheOtherRoles.zip
 $installPath = Get-ItemProperty -Path 'HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\Steam App 945360' | select-object -ExpandProperty InstallLocation
-if (Test-Path -Path .\"Among Us"){
-    xcopy /i /s /c .\"Among Us" $targetFolder
-    start $targetFolder
-} elseif (Test-Path -Path $installPath) {
-    xcopy /i /s /c $installPath $targetFolder
-    start $targetFolder
+if (Test-Path -Path $installPath) {
+    robocopy $installPath $folderName /e /b 
+    start $folderName
+} elseif (Test-Path -Path .\"Among Us"){
+    robocopy .\"Among Us" $folderName /e /vb /move
+    start $folderName
 } else {
-    echo "Copy your Among Us instillation folder into this folder as 'Among Us'"
+    echo "Copy your Among Us instillation folder into this folder as 'Among Us' and re-run this script"
     pause
 }
-del temp.zip
-del tmp
